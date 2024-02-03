ef00ba0731fa5538e4f97ae0955f59384f2933b0
toshiyakjp42 <toshiyakjp42@gmail.com>
2024-02-03 18:01:37 +0900

    Increment the counter by 2 per click

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var numOfClicks = 0;
 
-$(document).ready(function() {
-  $("#click-button").on("click", function() {
-    numOfClicks += 1;
+$(document).ready(function () {
+  $("#click-button").on("click", function () {
+    numOfClicks += 2;
     document.getElementById("click-counter").innerHTML = numOfClicks;
-  }); 
+  });
 });
